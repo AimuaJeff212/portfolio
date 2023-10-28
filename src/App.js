259c9388a4e373ef5bd7f116f404aab980a54001
osijeff <aimua_jeff@yahoo.com>
2023-10-28 09:41:53 +0200

    basic structure created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {Home} from './pages/Home'
+import About from './pages/About'
+import {Contact} from './pages/Contact'
+import Navbar from './components/Navbar'
 import './App.css';
 
 function App() {
   return (
-    
+    <BrowserRouter>
+    < Navbar />
+    <Routes>
+      <Route path='/'  element={ <Home></Home> }/>
+      <Route path='/about'  element={ <About></About> }/>
+      <Route path='/contact'  element={ <Contact></Contact> }/>
+    </Routes>
+    </BrowserRouter>
       
   );
 }
